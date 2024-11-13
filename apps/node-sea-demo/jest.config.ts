5206f16d287f9e1a42bc201e851f826349c4d249
getlarge <ed@getlarge.eu>
2024-11-13 08:38:26 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/HfiCxFcvsA

---
+++ apps/node-sea-demo/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'node-sea-demo',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/node-sea-demo',
+};
