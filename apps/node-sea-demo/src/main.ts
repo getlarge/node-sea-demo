826152412aa352e86203dc14ac23b7cf91e03568
getlarge <ed@getlarge.eu>
2024-11-17 09:50:59 +0100

    fix: workaround for undefined require.cache in SEA

--- apps/node-sea-demo/src/main.ts
+++ apps/node-sea-demo/src/main.ts
@@ -1,3 +1,12 @@
+import sea from 'node:sea';
+
+if (sea.isSea()) {
+  // workaround for undefined require.cache https://github.com/nodejs/node/issues/49163
+  const { createRequire } = require('node:module');
+  // eslint-disable-next-line no-global-assign
+  require = createRequire(__filename);
+}
+
 import Fastify from 'fastify';
 import { app } from './app/app';
 import { getAssetsDir } from './app/helpers';
